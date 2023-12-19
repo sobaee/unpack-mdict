1f073ef56ea14a6d76b5f9552a98ce8525cfbef7
sobaee <65910585+sobaee@users.noreply.github.com>
2023-12-19 19:17:00 +0300

    Add files via upload

--- unpack-mdict.sh
+++ unpack-mdict.sh
@@ -7,7 +7,21 @@
 # pip3 install mdict-utils python-lzo
 #
 # pyglossary better to be installed from a local folder with: python setup.py install (better to use my ready pyglossary zip file)
-#
+
+
+if command -v python3 >/dev/null 2>&1; then
+        if command -v mdict >/dev/null 2>&1; then
+            echo -e "All dependencies are ready!\n"
+        else
+            echo "ERROR: mdict not found! Run 'pip3 install mdict-utils'!"
+            exit 1
+        fi
+else
+    echo "ERROR: python not installed! Download and install from https://www.python.org/downloads"
+    exit 1
+fi
+
+
 src=""
 read -p "Input file (ex. dictionary.mdx): " src
 
@@ -22,6 +36,9 @@ echo 'All done!'
 
 fi
 
+rm -v "${src%.*}.mdx.title.html"
+rm -v "${src%.*}.mdx.description.html"
+
 if [ -e "${src%.*}.mtxt" ]; then
     read -p "${src%.*}.mtxt already exists! OVERWRITE? (y/n) " answer
     if [[ $answer =~ ^[Yy]$ ]]; then
@@ -35,7 +52,6 @@ mv "${src%.*}.mdx.txt" "${src%.*}.mtxt"
 
 
 
-rm -v "${src%.*}.mdx.title.html"
-rm -v "${src%.*}.mdx.description.html"
+
 
 echo 'All done!'
